8c2808a709ad7eacee706709567e85b95b7af456
Daniel Neugebauer <dneuge@energiequant.de>
2025-05-09 18:29:23 +0200

    fix unit tests

--- src/test/java/de/energiequant/limamf/connector/ConfigurationTest.java
+++ src/test/java/de/energiequant/limamf/connector/ConfigurationTest.java
@@ -2,19 +2,23 @@ package de.energiequant.limamf.connector;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.mockito.Mockito.mock;
 
 import java.util.Optional;
 
 import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
 import org.junit.jupiter.api.Test;
 
+import de.energiequant.apputils.misc.DisclaimerState;
+
 class ConfigurationTest {
     @Test
     void testCreateFromDefaults_always_doesNotFail() {
         // arrange
+        DisclaimerState disclaimerState = mock(DisclaimerState.class);
 
         // act
-        ThrowingCallable action = () -> Configuration.createFromDefaults();
+        ThrowingCallable action = () -> Configuration.createFromDefaults(disclaimerState);
 
         // assert
         assertThatCode(action).doesNotThrowAnyException();
@@ -23,7 +27,8 @@ class ConfigurationTest {
     @Test
     void testCreateFromDefaults_always_hasNoAcceptedDisclaimer() {
         // arrange
-        Configuration config = Configuration.createFromDefaults();
+        DisclaimerState disclaimerState = mock(DisclaimerState.class);
+        Configuration config = Configuration.createFromDefaults(disclaimerState);
 
         // act
         Optional<String> result = config.getAcceptedDisclaimer();
